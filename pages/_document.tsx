5233cf4d4b975e33a6c1e6b2fe0e814c0edd2290
Jesus M Hernandez <jesus.herzo@outlook.com>
2022-02-09 00:59:27 -0600

    feat:  added cover photo and google font

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="La boda de Zyanya y Jesus" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import jumbotronLouvre from "../public/jumbotronLouvre.jpg";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Bienvenidos a la boda de Zyanya y Jesús
-        </h1>
-        <h3 className={styles.description}>Estamos en construcción...</h3>
-      </main>
-      <footer className={styles.footer}>Con amor por J & Z</footer>
-    </div>
+    <>
+      <Head>
+        <title>La boda de Zyanya y Jesus</title>
+      </Head>
+      <div>
+        <Image
+          src={jumbotronLouvre}
+          alt="Portada con Jesus y Zyanya"
+          layout="fill"
+          objectFit="cover"
+          placeholder="blur"
+          priority
+        />
+        <main className={styles.main}>
+          <h1 className={styles.title}>Zyanya & Jesús</h1>
+          <h3 className={styles.description}>05 - Nov - 2022</h3>
+        </main>
+        <footer className={styles.footer}> Hecho con ❤️ por J&Z</footer>
+      </div>
+    </>
   );
 };
 
